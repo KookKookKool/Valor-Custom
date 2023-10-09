8cd829ff39a3c0a7ddf15e52144a4dc3e44e24f5
Kookkookkool <sorakom.aunglan@gmail.com>
2023-10-09 17:09:18 +0700

    card

--- my-app/src/Components/Products.js
+++ my-app/src/Components/Products.js
@@ -5,13 +5,13 @@ export default function Products() {
   return (
     <div>
         <div className='Productsitem'>
-          <div className='Card'>
+          <div className='Card1'>
             <img id="Logo1" src={require('./t-shirt.png')} alt="img" />
             </div>
-            <div className='Card'>            
+            <div className='Card2'>            
             <img id="Logo2" src={require('./combatshirt.png')} alt="img" />
             </div>
-            <div className='Card'>    
+            <div className='Card3'>    
             <img id="Logo3" src={require('./cup.png')} alt="img" />
             </div>
 
