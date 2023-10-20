f32b8aafcd56fae1e79efb4d823355e0d74af010
Kookkookkool <sorakom.aunglan@gmail.com>
2023-10-20 15:57:33 +0700

    Revert "save"
    This reverts commit faccbf245504303051f4d79e2e345cb7e5c6032c.

--- my-app/src/Upload/Upload1.js
+++ my-app/src/Upload/Upload1.js
@@ -1,28 +1,12 @@
 import React, { useState } from "react";
 import "./Upload1.css"
 
-function Upload({ onUpload, allowedTypes, maxSize }) {
+function Upload({ onUpload }) {
   const [file, setFile] = useState();
-  const [error, setError] = useState(null);
 
   function handleChange(e) {
     const selectedFile = e.target.files[0];
-
     if (selectedFile) {
-      if (allowedTypes && allowedTypes.length > 0) {
-        const fileExtension = selectedFile.type.split('/')[1];
-        if (!allowedTypes.includes(fileExtension)) {
-          setError("ประเภทของไฟล์ไม่ถูกต้อง");
-          return;
-        }
-      }
-
-      if (maxSize && selectedFile.size > maxSize) {
-        setError("ขนาดของไฟล์ใหญ่เกินไป");
-        return;
-      }
-
-      setError(null);
       setFile(URL.createObjectURL(selectedFile));
       onUpload(selectedFile);
     }
@@ -33,15 +17,13 @@ function Upload({ onUpload, allowedTypes, maxSize }) {
       <input
         type="file"
         id="fileInput"
-        accept={allowedTypes && allowedTypes.length > 0 ? allowedTypes.map(type => `.${type}`).join(',') : "image/*"}
+        accept="image/*"
         onChange={handleChange}
         style={{ display: "none" }}
       />
       <label htmlFor="fileInput" className="upload-button center-text">
         อัพโหลดรูปภาพ
       </label>
-      {error && <p className="error-message">{error}</p>}
-
     </div>
   );
 }
