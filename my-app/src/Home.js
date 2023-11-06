a43eda09cf5b77f6401c5189d0ee132b7d3e437a
Kookkookkool <sorakom.aunglan@gmail.com>
2023-11-06 17:36:05 +0700

    save header Overlay

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,15 +3,19 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import Slider from "./Slider/Slider";
 import Footer from "./Footer";
+import CTicon from "./Asset/icon/ValorCustom.png";
 
 export default function Home() {
   return (
     <>
       <div className="video-container">
         <Link to="/main">
+
           <video className="Video" autoPlay loop muted playsInline>
             <source src="/SQ1_2.mp4" type="video/mp4" />
           </video>
+          <img src={CTicon} alt="icon" className="image-overlay" />
+
           <div className="centered-button">
             <button>CUSTOM DESIGN</button>
           </div>
