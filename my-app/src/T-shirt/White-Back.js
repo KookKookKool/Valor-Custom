808088f048a1b6118005c53d1caa6ec10fce7da9
Kookkookkool <sorakom.aunglan@gmail.com>
2023-11-15 17:22:03 +0700

    Update White-Back.js

--- my-app/src/T-shirt/White-Back.js
+++ my-app/src/T-shirt/White-Back.js
@@ -7,7 +7,10 @@ import WhiteBackMockup from "../Asset/T-shirt/White-Back.png";
 import MenuWhiteBack from "../Components/White/MenuWhiteBack";
 import Upload from "../Upload/Upload1";
 import FieldFull from "../Asset/T-shirt/Field-Full-Black.png"; // นี่คือการ import 'FieldFull'
+
 import ImageSaveButton from "../Custom-Design/ImageSaveButton";
+
+import "../Components/Products.css";
 
 function WhiteBack({ setProductsitemOpen }) {
   const [uploadedImage, setUploadedImage] = useState(null);
@@ -18,7 +21,7 @@ function WhiteBack({ setProductsitemOpen }) {
   const [imageStyle, setImageStyle] = useState({
     width: "54%", // ค่าเริ่มต้นสำหรับ A3
 
-    objectPosition: "left 0px top 182px",
+    objectPosition: "left 0px top 202px",
   });
 
   const handleImageStyleChange = (selectedOption) => {
@@ -31,38 +34,51 @@ function WhiteBack({ setProductsitemOpen }) {
         case "1, default":
           setImageStyle({
             width: "54%",
-            objectPosition: "left 0px top 182px",
+            objectPosition: "left 0px top 202px",
           });
           break;
         case "2":
           setImageStyle({
             width: "48%",
 
-            objectPosition: "left 0px top 182px",
+            objectPosition: "left 0px top 202px",
           });
           break;
         case "3":
           setImageStyle({
             width: "40%",
 
-            objectPosition: "left 0px top 182px",
+            objectPosition: "left 0px top 202px",
           });
           break;
         case "4":
           setImageStyle({
             width: "28%",
 
-            objectPosition: "left 0px top 182px",
+            objectPosition: "left 0px top 202px",
           });
           break;
-
-          default:
-            // กระทำเมื่อไม่มี case ไหนตรงกับ imageStyleOption
-            break;
-        }
+        case "5":
+          setImageStyle({
+            width: "18%",
+            paddingLeft: "24%",
+            objectPosition: "left 0px top 212px",
+          });
+          break;
+        case "6":
+          setImageStyle({
+            width: "14%",
+            paddingLeft: "29%",
+            objectPosition: "left 0px top 218px",
+          });
+          break;
+        default:
+          // กระทำเมื่อไม่มี case ไหนตรงกับ imageStyleOption
+          break;
       }
-    }, [showDropdown, imageStyleOption]);
-    
+    }
+  }, [showDropdown, imageStyleOption]);
+
   const handleImageUpload = (selectedImage) => {
     setUploadedImage(URL.createObjectURL(selectedImage));
   };
@@ -70,23 +86,24 @@ function WhiteBack({ setProductsitemOpen }) {
   const handleSaveImage = () => {
     setSavedImage(uploadedImage);
   };
+
   const handleGoBack = () => {
     const confirmed = window.confirm(
-      "คุณต้องการย้อนกลับ ?"
+      "คุณต้องการย้อนกลับ โดยรายการจะไม่ถูกบันทึกหรือไม่ ?"
     );
     if (confirmed) {
-      window.location.href = "/T-shirt/White-Front";
+      window.location.href = "/Main";
     }
   };
-
 
   return (
     <>
       <div className="container">
         <img id="Logo" src={require("../logo.png")} alt="img" />
         <div className="Frame1">
-          <h3 className="CenteredHeader">CUSTOM DESIGN</h3>
-
+          <div>
+        <h3 className="CenteredHeader">CUSTOM DESIGN</h3>
+        </div>
           <div className="Box">
             <div className="Box2">
               <button id="BntBack" onClick={handleGoBack}>
@@ -96,11 +113,10 @@ function WhiteBack({ setProductsitemOpen }) {
                 <Navigation />
               </div>
             </div>
-
             <div className="FieldCustom">
-            <div className="CustomFront">
-              <img id="MockupBack" src={WhiteBackMockup} alt="Mockup" />
-              {uploadedImage ? (
+              <div className="CustomFront">
+                <img id="MockupFront1" src={WhiteBackMockup} alt="Mockup" />
+                {uploadedImage ? (
                   <img
                     id="FieldUpload"
                     src={uploadedImage}
@@ -108,16 +124,38 @@ function WhiteBack({ setProductsitemOpen }) {
                     style={imageStyle}
                   />
                 ) : (
-                  <img id="FieldDesign2" src={FieldFull} alt="FieldDesign" />
+                  <img id="FieldDesign" src={FieldFull} alt="FieldDesign" />
                 )}
+              </div>
+
+              <div className="Box5">
+                <MenuWhiteBack />
+                {showDropdown && (
+                  <div className="dropdown">
+                    <label>ขนาด:</label>
+                    <select
+                      value={imageStyleOption}
+                      onChange={(e) => handleImageStyleChange(e.target.value)}
+                    >
+                      <option value="1, default">A3</option>
+                      <option value="2">A4</option>
+                      <option value="3">A5</option>
+                      <option value="4">A6</option>
+                      <option value="5">A7</option>
+                      <option value="6">A8</option>
+                    </select>
+                  </div>
+                )}
+              </div>
+              <div className="Box6">
+                <Upload onUpload={handleImageUpload} />
+                <ImageSaveButton
+                  onSave={handleSaveImage}
+                  savedImage={savedImage}
+                />
+              </div>
             </div>
-            <Upload onUpload={handleImageUpload} />
-            <ImageSaveButton
-                onSave={handleSaveImage}
-                savedImage={savedImage}
-              />
-            </div>
-            <div className="Box3">
+            <div className="Box4">
               <MenuWhiteBack />
               {showDropdown && (
                 <div className="dropdown">
@@ -130,14 +168,17 @@ function WhiteBack({ setProductsitemOpen }) {
                     <option value="2">A4</option>
                     <option value="3">A5</option>
                     <option value="4">A6</option>
-
+                    <option value="5">A7</option>
+                    <option value="6">A8</option>
                   </select>
                 </div>
+                
               )}
             </div>
           </div>
         </div>
       </div>
+
     </>
   );
 }
