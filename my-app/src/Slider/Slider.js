fedad56596a2678295cb80732a455cc149fc78db
KookKookKool <135952919+KookKookKool@users.noreply.github.com>
2024-03-05 14:56:09 +0700

    save slider hover detail

--- my-app/src/Slider/Slider.js
+++ my-app/src/Slider/Slider.js
@@ -1,22 +1,7 @@
 import React, { useState } from 'react';
 import './Slider.css';
-import M1 from '../Asset/Mockup/1.png';
-import M2 from '../Asset/Mockup/2.png';
-import M3 from '../Asset/Mockup/3.png';
-import M4 from '../Asset/Mockup/4.png';
-import M5 from '../Asset/Mockup/5.png';
-import M6 from '../Asset/Mockup/6.png';
-import M7 from '../Asset/Mockup/7.png';
-import M8 from '../Asset/Mockup/8.png';
-import M9 from '../Asset/Mockup/9.png';
-import M10 from '../Asset/Mockup/10.png';
-import M11 from '../Asset/Mockup/11.png';
-import M12 from '../Asset/Mockup/12.png';
-import M13 from '../Asset/Mockup/13.png';
-import M14 from '../Asset/Mockup/14.png';
-import M15 from '../Asset/Mockup/15.png';
-import M16 from '../Asset/Mockup/16.png';
-import M17 from '../Asset/Mockup/17.png';
+
+import Prd1 from '../Slider/Product/prd1';
 
 function Slider() {
   // State to control the animation play state
@@ -46,56 +31,57 @@ function Slider() {
         }}
       >
 	<div className="logo-slide-track">
+
 		<div className="slide">
-			<img src={M1} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M2} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M3} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M4} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M5} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M6} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M7} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M8} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M9} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M10} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M11} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M12} alt="" />
+			<Prd1/>
 		</div>
+
 		<div className="slide">
-			<img src={M13} alt="" />
-		</div>
-		<div className="slide">
-			<img src={M14} alt="" />
-		</div>
-		<div className="slide">
-			<img src={M15} alt="" />
-		</div>   
-		<div className="slide">
-			<img src={M16} alt="" />
-		</div>
-		<div className="slide">
-			<img src={M17} alt="" />
+			<Prd1/>
 		</div>
 		
 	</div>
